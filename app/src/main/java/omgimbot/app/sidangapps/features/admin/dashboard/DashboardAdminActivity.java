d9623e39fe365471ae61f4647a91efdd533c703f
bram26 <bram.17411071@student.ubl.ac.id>
2020-12-02 20:19:58 +0700

    logo

---
+++ app/src/main/java/omgimbot/app/sidangapps/features/admin/dashboard/DashboardAdminActivity.java
@@ -0,0 +1,16 @@
+package omgimbot.app.sidangapps.features.admin.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import omgimbot.app.sidangapps.R;
+
+public class DashboardAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_admin);
+    }
+}
